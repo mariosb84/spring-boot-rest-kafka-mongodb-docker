1a16a91a7f69bec411ac1f540e24db5dafe58a23
mariosb84 <mariosb84@mail.ru>
2024-02-06 17:54:06 +0300

    "MySobesTasks"

--- commonlibs/src/main/java/testingMyProjects/ExeptionPrint.java
+++ commonlibs/src/main/java/testingMyProjects/ExeptionPrint.java
@@ -5,21 +5,30 @@ import java.io.IOException;
 /*СОБЕСЕДОВАНИЕ - ЗАДАЧА ПО EXEPTION - В КАКОМ ПОРЯДЕК ВЫВЕДУТСЯ EXEPTION*/
 public class ExeptionPrint {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         System.out.println("Start");
 
-      /*  try(method()) {
-            System.out.println( " new  www IO Exeption");
+        try {
+            System.out.println( " new  IO Exeption");
+            System.out.println( " new  1 IO Exeption");
+            System.out.println( " new  11 IO Exeption");
+            method();
+            System.out.println( " new  111 IO Exeption");
+            System.out.println( " new  1111 IO Exeption");
+            System.out.println( " new  11111 IO Exeption");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        } catch (IOException);
-
-        System.out.println( " new IO Exeption");*/
+        System.out.println( " new 222 IO Exeption");
+        System.out.println( " new 333 IO Exeption");
+        System.out.println( " new 444 IO Exeption");
     }
 
-    private   static void method() throws IOException {
+    private static void method() throws IOException {
 
-        throw new RuntimeException("IO Exeption");
+        throw new IOException("new method IO Exeption");
     }
 
 }
